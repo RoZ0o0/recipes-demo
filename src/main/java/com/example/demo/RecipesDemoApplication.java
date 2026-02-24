2dafc28455b7427e0289421654acd3d1a2362b22
Patryk Plizga <pplizga665@gmail.com>
2026-02-24 16:35:45 +0100

    chore: initial project setup with dependencies

---
+++ src/main/java/com/example/demo/RecipesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipesDemoApplication.class, args);
+	}
+
+}
